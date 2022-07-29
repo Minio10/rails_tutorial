b6d77d054ea75002d7fe3f5f434bfaf76b31a37f
Minio10 <herminiom10@gmail.com>
2022-07-29 11:49:55 +0100

    Add before action to prevent a logged in user to edit other user's info

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -3,9 +3,12 @@ require "test_helper"
 class UsersEditTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @other_user = users(:archer)
   end
 
   test "unsuccessful edit" do
+
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name: "",
@@ -19,6 +22,8 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
+
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     name = "Foo Bar"
@@ -33,4 +38,17 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal name, @user.name
     assert_equal email, @user.email
   end
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+      get edit_user_path(@user)
+      assert flash.empty?
+      assert_redirected_to root_url
+    end
+    test "should redirect update when logged in as wrong user" do
+      log_in_as(@other_user)
+      patch user_path(@user), params: { user: { name: @user.name,
+                                                email: @user.email } }
+      assert flash.empty?
+      assert_redirected_to root_url
+    end
 end
