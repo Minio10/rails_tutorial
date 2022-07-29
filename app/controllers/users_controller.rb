5b72b37528d21b203d2ba550ef55c9128807d3e5
Minio10 <herminiom10@gmail.com>
2022-07-29 12:00:18 +0100

    Create test for friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,4 +50,10 @@ class UsersController < ApplicationController
         redirect_to login_url
       end
     end
+
+    # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 end
