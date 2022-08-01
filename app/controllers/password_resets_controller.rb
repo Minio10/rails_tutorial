b11750a013006350d366b115acfe1ed06361e860
Minio10 <herminiom10@gmail.com>
2022-08-01 14:59:07 +0100

    Update user mailer preview

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -6,6 +6,7 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
       @user.create_reset_digest
+      # byebug
       @user.send_password_reset_email
       flash[:info] = "Email sent with password reset instructions"
       redirect_to root_url
