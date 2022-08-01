b443b50d238fe5d45d508d4bf05fd270c56612ef
Minio10 <herminiom10@gmail.com>
2022-08-01 09:27:18 +0100

    Create migration for users

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
   get 'users/new', as: 'signup'
   resources :users
+  resources :account_activations, only: [:edit]
+
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
