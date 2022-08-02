87a359331378392efaf4fd38d14fcd12b23d4f97
Minio10 <herminiom10@gmail.com>
2022-08-02 16:12:38 +0100

    add following and followers numbers to the frontend

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,11 @@ Rails.application.routes.draw do
   get  '/about', to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get 'users/new', as: 'signup'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
